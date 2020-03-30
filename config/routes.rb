d20c09960f8398f808e5fec086a76242d2c2cf69
Perry Iler <piler1@my.westga.edu>
2020-03-30 16:54:58 -0400

    Added seed data to table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  root 'restaurants#index', as: 'restaurants_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+Restaurant.delete_all
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'restaurants.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Restaurant.new
+  t.name = row['name']
+  t.location = row['location']
+  t.willSplit = row['willSplit']
+  t.wontSplit = row['wontSplit']
+
+  t.save
+  puts "#{t.name} saved"
+end
+
+puts "There are now #{Restaurant.count} rows in the Restaurant table"
